ac6be0c67a0c8bc28cd5d851933f5c261d26b000
Carolin Brandt <carolin.brandt@tum.de>
2019-08-26 11:16:07 +0200

    fixing files not properly downloaded during evaluation data collection

--- evaluation/run-evaluation.rb
+++ evaluation/run-evaluation.rb
@@ -1,13 +1,16 @@
 # run the various evaluation tasks
 
-program = "BuildFailureReason/C/git@git"
-selection = "random"
+example_set = "BuildFailureReason/C/git@git"
+selection = "chronological"
 test_count = 1
 learning_step_count = 5
+technique = "pbe"
 
-run_ir_eval = "Rscript ../r-extractions/information-retrieval.R evaluate --program #{program} --selection #{selection} --test-count #{test_count} --learning-step-count #{learning_step_count} --verbose"
+run_ir_eval = "Rscript ../r-extractions/information-retrieval.R evaluate --program #{example_set} --selection #{selection} --test-count #{test_count} --learning-step-count #{learning_step_count} --verbose"
 
-command_to_run = run_ir_eval
+run_eval = "ruby ../tool/run-extraction.rb -a evaluate -t #{technique} -e #{example_set} -s #{selection} -l #{learning_step_count} -c #{test_count}"
+
+command_to_run = run_eval
 
 puts "running '#{command_to_run}'"
 puts `#{command_to_run}`
