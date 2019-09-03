f8a521d2d122d16186536ab90594ba13a5011e74
Carolin Brandt <carolin.brandt@tum.de>
2019-09-03 11:39:57 +0200

    packages for r-extractions, dockerfile for pbe

---
+++ r-extractions/install_packages.R
@@ -0,0 +1,18 @@
+r = getOption("repos")
+r["CRAN"] = "http://cran.us.r-project.org"
+options(repos = r)
+
+install.packages("optigrab")
+
+install.packages("XML")
+install.packages("methods")
+install.packages("graphics")
+install.packages("stringdist")
+install.packages("ggplot2")
+install.packages("svglite")
+install.packages("dplyr")
+install.packages("text2vec")
+install.packages("stringi")
+install.packages("stringr")
+install.packages("purrr")
+
