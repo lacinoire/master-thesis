ae0bd0110d617e70e79ce37122110d20c2fdcebf
Carolin Brandt <carolin.brandt@tum.de>
2020-03-26 20:41:31 +0100

    replication zip creation for replication repo

--- zip-replication-package.sh
+++ zip-replication-package.sh
@@ -1,2 +1,14 @@
 #! /bin/bash
-zip -r crt-replication.zip evaluation pbe-extraction-buildlogs r-extractions tool replication.md
+
+mkdir chunk-retrieval-replication
+
+cp -r evaluation chunk-retrieval-replication/evaluation
+cp -r pbe-extraction-buildlogs chunk-retrieval-replication/pbe-extraction-buildlogs
+cp -r r-extractions chunk-retrieval-replication/r-extractions
+cp -r tool chunk-retrieval-replication/tool
+cp replication.md chunk-retrieval-replication/readme.md
+cp .gitignore chunk-retrieval-replication/.gitignore
+
+zip -r chunk-retrieval-replication.zip chunk-retrieval-replication
+
+rm -rf chunk-retrieval-replication
